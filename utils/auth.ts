b4b98a11beeca51b57b725daacd0c4566681b70c
lxy <594328762@qq.com>
2025-07-21 15:41:37 +0800

    进入首页需登录

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,5 +1,4 @@
 export function get_auth_status(context) {
-    var dopath = context.request.url
     var headers = new Headers(context.request.headers);
     if(!headers.get('Authorization'))return false
     const Authorization=headers.get('Authorization').split("Basic ")[1]
